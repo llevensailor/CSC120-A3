c357c6bbc3ea48bc5a438fabe8527d36d9f0a2d2
Leah Levensailor <llevensailor@smith.edu>
2024-09-25 12:43:22 -0400

    All Done

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,104 @@
+import java.util.Scanner;
 class Conversation {
+/**
+ * 
+ * @param arguments
+ * ChatBot takes in input from person and has a conversation then gives
+ * transcript of the conversation. 
+ */
+
 
   public static void main(String[] arguments) {
     // You will start the conversation here.
+    Scanner input = new Scanner(System.in); 
+    System.out.println("How many rounds?");
+    int rounds = input.nextInt();
+     
+    //Transcript string holder and response string holder 
+    String [] transcript = new String[2 * rounds + 3];
+    String newResponse; 
+    
+    System.out.println("Hi! What would you like to talk about?");
+    transcript[0] = "Hi! What would you like to talk about?";
+    String response = input.nextLine();
+    response = input.nextLine(); 
+    //add response to the transcript 
+    transcript[1] = response; 
+    
+    for(int i = 0; i < rounds; i++)
+    { 
+      //Mirroring
+      newResponse = response; 
+      if(response.contains("I"))
+      { 
+        newResponse = newResponse.replace("I", "You");
+      
+      }
+    if(newResponse.contains("me"))
+    { 
+      newResponse = newResponse.replace("me", "you");
+      
+    }
+    if(newResponse.contains("am"))
+    { 
+      newResponse = newResponse.replace("am", "are");
+      
+    }
+    if(newResponse.contains("you"))
+    { 
+      newResponse = newResponse.replace("you", "I");
+      
+    }
+    if(newResponse.contains("my"))
+    { 
+      newResponse = newResponse.replace("my", "your");
+      
+    }
+     if(newResponse.contains("your"))
+    { 
+      newResponse = newResponse.replace("your", "my");
+      
+    }
+    if(newResponse.contains("."))
+    { 
+      newResponse = newResponse.replace(".", "?");
+      
+    }
+    //Randomly replying with a different response
+    else 
+    { 
+      double num = Math.random(); 
+      if (num < .25)
+      { newResponse = "Oh yeah?"; }
+      else if (num < .5)
+      { newResponse = "That's cool."; }
+      else if (num < .75)
+      { newResponse = "Wow!";}
+      else 
+      { newResponse = "Mhm"; }
+    }
+    System.out.println(newResponse);
+    transcript[2 * i + 1] = response;
+    transcript[2 * i + 2] = newResponse; 
+
+    response = input.nextLine();
+    }
+    System.out.println("Thanks for talking!"); 
+// makes sure the last string in response gets added 
+    transcript[transcript.length - 2] = response; 
+    transcript[transcript.length - 1] = "Thanks for talking!";
+    System.out.println();
+    System.out.println();
+    System.out.println("Transcript:");
+    for (String line : transcript)
+    { System.out.println(line);}
+
+
+
+
+
+
+
+
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+I talked through the transcript with Frankie and I talked to my stuffed dinosaur when I was coding by myself.
 - Which session(s) of TA / office hours did you attend?
+I attened Ab's Monday office hours. Most of the questions I was able to answer myself but I needed to ask the question outloud before I could figure out an answer. 
 - What are your initial impressions of Java? 
+I have experience with Java because I took AP Computer Science A so I am very comfortable with Java. 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+Java and Python are very similar in that they go about the same way to do things but Python seems to be a very stripped version of Java. My only experience with Python was our first coding experience. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I would advice someone to do what I did which was look at the small wins. I didn't focus on getting each part of the rubric to work all at once, I broke it into chunks and worked on specific parts. It took me a little to come up with the final equation for the number of elements in the transcript. I had trouble with mirroring at first but then I was able to fix my mistake easily and got it to work. My transcript worked from the beginning but it was skipping some of the inputs so it took me a little to figure out why and get everything that needed to be in it in the right order. 
